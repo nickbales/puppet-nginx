ce54a4f604025ff806ae466fda5463663383987c
Nick Bales <nick.bales@rackspace.com>
2018-11-27 21:45:22 +0000

    install and service

---
+++ manifests/service.pp
@@ -0,0 +1,19 @@
+class nginx::service(
+  String $ensure       = $::nginx::config_ensure,
+  String $service_name = $::nginx::service_name,
+  String $pattern      = $::nginx::service_pattern,
+  Bool $enable         = $::nginx::service_enable,
+  Bool $hasstatus      = $::nginx::service_hasstatus,
+  Bool $hasrestart     = $::nginx::service_hasrestart,
+) {
+  
+  service { 'nginx_service':
+    ensure     => $ensure,
+    name       => $service_name,
+    enable     => $enable,
+    hasstatus  => $hasstatus,
+    hasrestart => $hasrestart,
+    pattern    => $pattern,
+  }
+
+}
